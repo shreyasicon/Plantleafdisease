c91d943646eb367fcbdcc1da89854bc1764a59d6
SAI SHREYAS G H <84716993+shreyasicon@users.noreply.github.com>
2023-04-16 21:54:10 +0530

    Update plant.js

--- plant.js
+++ plant.js
@@ -1,25 +1,27 @@
-const form = document.getElementById("upload-form");
-const resultDiv = document.getElementById("result");
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("upload-form");
+    const resultDiv = document.getElementById("result");
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
 
-    const formData = new FormData(form);
+        const formData = new FormData(form);
 
-    fetch("/upload", {
-            method: "POST",
-            body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.error) {
-                resultDiv.innerHTML = `<p id="error">${data.error}</p>`;
-            } else {
-                resultDiv.innerHTML = `<p>${data.result}</p>`;
-            }
-        })
-        .catch(error => {
-            console.error(error);
-            resultDiv.innerHTML = `<p id="error">An error occurred while detecting the plant disease.</p>`;
-        });
+        fetch("/upload", {
+                method: "POST",
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    resultDiv.innerHTML = `<p id="error">${data.error}</p>`;
+                } else {
+                    resultDiv.innerHTML = `<p>${data.result}</p>`;
+                }
+            })
+            .catch(error => {
+                console.error(error);
+                resultDiv.innerHTML = `<p id="error">An error occurred while detecting the plant disease.</p>`;
+            });
+    });
 });
